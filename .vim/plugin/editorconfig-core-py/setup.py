87c02dcd1f1cd9b82c5cb8cdec8412d8b502b532
Jason Weir <jweir@dropship.com>
2014-01-22 11:20:26 -0700

    Updated editorconfig, git_template hooks, and some tweaks to rc files

--- .vim/plugin/editorconfig-core-py/setup.py
+++ .vim/plugin/editorconfig-core-py/setup.py
@@ -6,14 +6,13 @@ setup(
     version=editorconfig.__version__,
     author='EditorConfig Team',
     packages=['editorconfig'],
-    namespace_packages=['editorconfig'],
     url='http://editorconfig.org/',
     license='LICENSE.txt',
     description='EditorConfig File Locator and Interpreter for Python',
     long_description=open('README.rst').read(),
     entry_points = {
         'console_scripts': [
-            'editorconfig.py = editorconfig.main:main',
+            'editorconfig = editorconfig.main:main',
         ]
     },
 )
