b6e4ae71b90a231a6644e451b226d32cc5f4a134
Jason Weir <jason@rata.com>
2024-02-23 17:59:24 -0700

    Add some python aliases
    They were sitting around. I don't use them much, but every so often I
    do. Why not keep them. They aren't hurting anyone, right?

--- .profile.d/python.sh
+++ .profile.d/python.sh
@@ -1,1 +1,23 @@
 export PYTHONDONTWRITEBYTECODE=1
+
+# Pylint should use my config by default
+alias pylint="pylint --rcfile=/Users/jason/pyproject.toml"
+
+# Unit testing
+alias cover="coverage run --branch -m --omit='*test*' --source=. pytest -s; coverage report; coverage html &"
+alias pytest="PYTHONPATH=. pytest"
+function wunittest(){
+    # TODO: add param for no network
+    # TODO: add param for coverage
+    # TODO: allow pytest params (i.e. specific file, negating watchtest)
+    find **/*.py | entr -cs "PYTHONPATH=. sandbox-exec -f ~/no-network.sandbox coverage run --branch -m --omit='*test*' --source=. pytest -s && coverage html"
+}
+alias ,wunittest=wunittest
+function watchtest() {
+    # TODO: add param for no network
+    # TODO: add param for disable-warnings
+    # TODO: add param for coverage
+    # TODO: allow pytest params
+    ls **/*.py | entr -cs "PYTHONPATH=. pytest -s --disable-warnings $*"
+}
+alias ,watchtest=watchtest
